7c4af2031c7e6f43de3da4b43aa80999f1027988
Ivan Hdz <honter1997@gmail.com>
2021-01-11 15:32:25 -0600

    Initial commit

--- navegador-seguro/dist/main/sistema-operativo/maquina-virtual/VMCheckResult.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VMCheckResult = void 0;
-class VMCheckResult {
-}
-exports.VMCheckResult = VMCheckResult;
-//# sourceMappingURL=VMCheckResult.js.map
