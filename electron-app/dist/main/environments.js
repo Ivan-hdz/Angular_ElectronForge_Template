52911b9b0ee1758be299d9cb954938b5efc15f7b
Ivan Hdz <honter1997@gmail.com>
2021-01-11 16:02:40 -0600

    Template v1.0.0

--- electron-app/dist/main/environments.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environments = void 0;
-exports.environments = {
-    develop: true,
-    rootDir: __dirname
-};
-//# sourceMappingURL=environments.js.map
